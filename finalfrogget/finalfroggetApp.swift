2b66f12c713bdcfd59a8fe16abbb1cb0bed501ac
MJ210624 <mauricio.ferreira70@aluno.ifce.edu.br>
2025-12-05 14:50:49 -0300

    Initial Commit

---
+++ finalfrogget/finalfroggetApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalfroggetApp.swift
+//  finalfrogget
+//
+//  Created by user on 05/12/25.
+//
+
+import SwiftUI
+
+@main
+struct finalfroggetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
