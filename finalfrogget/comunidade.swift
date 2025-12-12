824420d2f2eeff4dd79f9de061fd5e213dc5d617
LorenF <loren.felix09@aluno.ifce.edu.br>
2025-12-12 14:33:03 -0300

    codigo comunidade sem o +, primeiro commit

---
+++ finalfrogget/comunidade.swift
@@ -0,0 +1,18 @@
+//
+//  comunidade.swift
+//  finalfrogget
+//
+//  Created by found on 09/12/25.
+//
+
+import SwiftUI
+
+struct comunidade: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    comunidade()
+}
